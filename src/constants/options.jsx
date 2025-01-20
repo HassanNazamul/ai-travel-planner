3f793dd4040548b6d7db92546a906605e9866508
Mohammed Nazamul Hassan <153888073+HassanNazamul@users.noreply.github.com>
2025-01-20 03:05:43 -0500

    basic user form is build and logic

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -1,39 +1,39 @@
 export const SelectTravelList = [
     {
         "id": 1,
-        "title": "just me",
+        "title": "Solo",
         "desc": "A solo traveller",
         "icon": "",
         "people": "1"
     },
     {
         "id": 2,
-        "title": "family trip",
-        "desc": "A family getaway",
-        "icon": "",
-        "people": "4"
-    },
-    {
-        "id": 3,
-        "title": "group vacation",
-        "desc": "A fun-filled group trip",
-        "icon": "",
-        "people": "8"
-    },
-    {
-        "id": 4,
-        "title": "business trip",
-        "desc": "A corporate journey",
-        "icon": "",
-        "people": "2"
-    },
-    {
-        "id": 5,
-        "title": "honeymoon",
+        "title": "Couple",
         "desc": "A romantic escape",
         "icon": "",
         "people": "2"
+    },
+    {
+        "id": 3,
+        "title": "Family Trip",
+        "desc": "A family getaway",
+        "icon": "",
+        "people": "4-6"
+    },
+    {
+        "id": 4,
+        "title": "Freinds Vacation",
+        "desc": "A fun-filled group trip",
+        "icon": "",
+        "people": "4-8"
     }
+    // {
+    //     "id": 4,
+    //     "title": "business trip",
+    //     "desc": "A corporate journey",
+    //     "icon": "",
+    //     "people": "2"
+    // },
 
 ]
 
@@ -42,19 +42,19 @@ export const SelectBudgetOptions = [
         "id": 1,
         "title": "Cheap",
         "desc": "Stay conscious of costs",
-        "icon": ""
+        "icon": "💵"
     },
     {
         "id": 2,
         "title": "Moderate",
         "desc": "A balance between comfort and price",
-        "icon": ""
+        "icon": "💰"
     },
     {
         "id": 3,
         "title": "Luxury",
         "desc": "Experience the best with no compromise",
-        "icon": ""
+        "icon": "🪙💰🪙"
     }
 
 ]
--- src/createtrip/CreateTrip.jsx
+++ src/createtrip/CreateTrip.jsx
@@ -1,13 +1,45 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useState } from "react"
+import { SelectBudgetOptions, SelectTravelList } from "@/constants/options";
+import { useEffect, useState } from "react"
 import GooglePlacesAutocomplete from "react-google-places-autocomplete"
 
 function CreateTrip() {
 
     const [place, setPlace] = useState();
 
+    const [formData, setFormData] = useState([]);
+
+    const handleInputChange = (name, value) => {
+
+        if (name == 'no:days' && value > 5) {
+            console.log('Please enter days < 5')
+        }
+
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        console.log(formData)
+    }, [formData])
+
+
+    const OnGenerateTrip = () => {
+        if (formData?.noOfdays > 5) {
+
+            // to show toast meathod
+
+            return;
+        }
+
+        console.log(formData)
+    }
+
     return (
-        <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+        <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 m-20 ">
 
             <h2 className="font-bold text-3xl">Tell us your travel preference</h2>
             <p className="mt-3 text-gray-500 text-xl">Just provide some basic info, and our trip planner will
@@ -15,6 +47,8 @@ function CreateTrip() {
 
 
             <div className="mt-20 flex flex-col gap-10">
+
+                {/* this is one is for destination */}
                 <div>
                     <h2 className="text-xl my-3 font-medium">What is your destination?</h2>
                     <GooglePlacesAutocomplete
@@ -23,17 +57,64 @@ function CreateTrip() {
                             place,
                             onChange: (value) => {
                                 setPlace(value);
+                                handleInputChange("location", value);
                                 // console.log(value)
                             }
                         }}
                     />
                 </div>
+
+                {/* this is for days */}
+                <div>
+                    <h2 className="text-xl my-3 font-medium">How many days?</h2>
+                    <Input placeholder={'Example: 3 Days'} type="number"
+                        onChange={(e) => handleInputChange("noOfdays", e.target.value)}
+                    />
+                </div>
+
+                {/* this is to get budget */}
+                <div>
+                    <h2 className="text-xl my-3 font-medium">What is your budget?</h2>
+                    <div className="grid grid-cols-3 gap-5 mt-5">
+                        {SelectBudgetOptions.map((item, index) => (
+
+                            <div key={index}
+                                onClick={() => handleInputChange("budget", item.title)}
+                                className="p-4 border rounded-lg hover:shadow-lg cursor-pointer">
+                                <h2 className="text-4xl"> {item.icon} </h2>
+                                <h2 className="font-bold text-lg"> {item.title} </h2>
+                                <h2 className="text-sm text-gray-500"> {item.desc} </h2>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+
+                {/* to get traveller and accompany details */}
+                <div>
+                    <h2 className="text-xl my-3 font-medium">Who do you plan on travelling with on your next trip</h2>
+                    <div className="grid grid-cols-3 gap-5 mt-5">
+                        {SelectTravelList.map((item, index) => (
+
+                            <div key={index}
+                                onClick={() => handleInputChange("traveller", item.people)}
+                                className="p-4 border rounded-lg hover:shadow-lg cursor-pointer">
+                                <h2 className="text-4xl"> {item.icon} </h2>
+                                <h2 className="font-bold text-lg"> {item.title} </h2>
+                                <h2 className="text-sm text-gray-500"> {item.desc} </h2>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
             </div>
 
-            <h2 className="text-xl my-3 font-medium">What is your destination?</h2>
-            <Input placeholder={'Example: 3 Days'} type="number"></Input>
+            <div className="my-10 justify-end flex">
+                <Button onClick={OnGenerateTrip}>Generate Trip</Button>
+            </div>
 
         </div>
+
     )
 }
 
